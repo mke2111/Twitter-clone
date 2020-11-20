b4f23d552e7bf52ea80595d6f034b9d9bbd005fb
mke2111 <roymke134@gmail.com>
2020-11-20 20:29:35 +0300

    add devise migration and style new form

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,7 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
 
   # GET /tweets
   # GET /tweets.json
